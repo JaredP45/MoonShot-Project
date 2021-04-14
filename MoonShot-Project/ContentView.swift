c1855990eca8dbad69f62b5db5814be7656de79f
Jared <paubelj45@gmail.com>
2021-04-14 09:21:28 -0500

    Initial Commit

---
+++ MoonShot-Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoonShot-Project
+//
+//  Created by student on 4/14/21.
+//  Copyright © 2021 IN185 BS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
