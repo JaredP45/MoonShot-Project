e8d0f67bbf5cac027c244291221c361c78f2027f
Jared <paubelj45@gmail.com>
2021-04-16 13:02:21 -0500

    Updated the project to show how many missions an astronaut has participated in, and added a toggle feature to allow the user to choose whether to display the launch date of the mission or the names of the astronauts in the mission.

--- MoonShot-Project/AstronautView.swift
+++ MoonShot-Project/AstronautView.swift
@@ -2,14 +2,29 @@
 //  AstronautView.swift
 //  MoonShot-Project
 //
-//  Created by student on 4/14/21.
+//  Created by Jared Paubel on 4/14/21.
 //  Copyright © 2021 IN185 BS. All rights reserved.
 //
 
 import SwiftUI
 
+
+
 struct AstronautView: View {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
     let astronaut: Astronaut
+    
+    var astronautMissions: [String] {
+        var temp = [String]()
+        for mission in missions {
+            for crew in mission.crew {
+                if astronaut.id == crew.name {
+                    temp.append(mission.displayName)
+                }
+            }
+        }
+        return temp
+    }
     
     var body: some View {
         GeometryReader { geometry in
@@ -20,14 +35,26 @@ struct AstronautView: View {
                         .scaledToFit()
                         .frame(width: geometry.size.width)
                     
+                    Text("Missions Participated in:")
+                        .font(.headline)
+                    ForEach(self.astronautMissions, id:\.self) { mission in
+                        Text(mission)
+                            .font(.title)
+                            .fontWeight(.heavy)
+                            .foregroundColor(Color.blue)
+                    }
+                    
                     Text(self.astronaut.description)
                         .padding()
                     .layoutPriority(1)
+                    
                 }
             }
         }
         .navigationBarTitle(Text(astronaut.name), displayMode: .inline)
     }
+    
+    
 }
 
 struct AstronautView_Previews: PreviewProvider {
--- MoonShot-Project/ContentView.swift
+++ MoonShot-Project/ContentView.swift
@@ -12,25 +12,44 @@ struct ContentView: View {
     let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
-    var body: some View {
-        NavigationView {
-            List(missions) { mission in
-                NavigationLink(destination: MissionView(mission: mission, astronauts: self.astronauts)) {
+    @State private var showLaunchDate = false
+    
+        var body: some View {
+            NavigationView {
+                List(missions) { mission in
                     
-                    Image(mission.image)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 44, height: 44)
-                    
-                    VStack(alignment: .leading) {
-                        Text(mission.displayName)
-                            .font(.headline)
-                        Text(mission.formattedLaunchDate)
-                    }
+                    NavigationLink(
+                        destination: MissionView(mission: mission, astronauts: self.astronauts),
+                        label: {
+                        
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 44, height: 44)
+                            
+                            VStack(alignment: .leading) {
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                
+                                Text(self.showLaunchDate ? mission.formattedLaunchDate: self.crewNames(actualMission: mission))
+                            }
+                        })
                 }
-            }
-        .navigationBarTitle("MoonShot")
+                .navigationBarTitle("MoonShot")
+                .navigationBarItems(trailing: Button(
+                    action: { self.showLaunchDate.toggle()
+                    }, label: {
+                        Text(showLaunchDate ? "Crew" : "Dates")
+                }))
         }
+    }
+    
+    func crewNames(actualMission: Mission) -> String {
+        var temp = [String]()
+        for crewName in actualMission.crew {
+            temp.append(crewName.name.capitalized)
+        }
+        return temp.joined(separator: ", ")
     }
 }
 
